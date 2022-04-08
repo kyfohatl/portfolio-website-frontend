d5eb1defad00b2004ac4ba2665b57f44c9438894
Ehsan <ehsan.soltani71@yahoo.com>
2022-04-09 00:01:33 +1000

    Added sign out functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,21 @@
-import { Link } from "react-router-dom"
+import { useCallback } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import Api from "../lib/Api"
 import Button from "./Button"
 
 import styles from "./Navbar.module.css"
 
 export default function Navbar() {
+  // Used for navigating with the react router
+  const navigate = useNavigate()
+
+  const onSignOutClick = useCallback(async () => {
+    // Sing out the user
+    await Api.signOut()
+    // Now navigate to the sign in page
+    navigate("/signin")
+  }, [navigate])
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.nav}>
@@ -18,9 +29,14 @@ export default function Navbar() {
         {Api.hasTokens()
           ?
           <li className={styles.buttonAuth}>
-            <Link to="/signin" className={styles.authLink}>
-              <Button text="Sign out" type={{ type: "submit" }} height="36px" width="80px" marginTop="0px" backgroundColor="#253C78" />
-            </Link>
+            <Button
+              text="Sign out"
+              type={{ type: "button", callBack: onSignOutClick }}
+              height="36px"
+              width="80px"
+              marginTop="0px"
+              backgroundColor="#253C78"
+            />
           </li>
           : [
             <li className={styles.buttonAuth}>
--- src/lib/Api.ts
+++ src/lib/Api.ts
@@ -3,4 +3,33 @@ export default class Api {
     if (localStorage.accessToken || localStorage.refreshToken) return true
     return false
   }
+
+  static deleteLocalTokens() {
+    localStorage.removeItem("accessToken")
+    localStorage.removeItem("refreshToken")
+  }
+
+  static async signOut() {
+    if (localStorage.refreshToken) {
+      try {
+        const response = await fetch("http://localhost:8000/auth/users/logout", {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({token: localStorage.refreshToken})
+        })
+
+        if (response.status !== 204) {
+          // TODO
+          console.error("Error: Refresh token is invalid")
+        } else {
+          Api.deleteLocalTokens()
+        }
+      } catch (err) {
+        // TODO
+        console.error("Error: could not submit signout request to API", err)
+      }
+    }
+  }
 }
