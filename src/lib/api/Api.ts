8fbc9a2081954f54907cceaaec8991f15d864216
Ehsan <ehsan.soltani71@yahoo.com>
2022-04-18 02:41:07 +1000

    Abstracted fetching with authentication functionality away into a separate function

--- src/lib/api/Api.ts
+++ src/lib/api/Api.ts
@@ -1,4 +1,4 @@
-import { areTokensPresentInStorage, redirectToSignInAndClearData, refreshTokens } from "./auth.api"
+import { fetchWithAuth, redirectToSignInAndClearData } from "./auth.api"
 
 export default class Api {
   static async signOut() {
@@ -9,7 +9,7 @@ export default class Api {
           headers: {
             "Content-Type": "application/json"
           },
-          body: JSON.stringify({token: localStorage.refreshToken})
+          body: JSON.stringify({ token: localStorage.refreshToken })
         })
 
         if (response.ok) {
@@ -27,40 +27,11 @@ export default class Api {
 
   // Saves the given blog to the database
   static async saveBlog(html: string, css: string) {
-    // Ensure tokens are present in storage
-    if (!await areTokensPresentInStorage()) return
-
     try {
-      const response = await fetch("http://localhost:8000/blog/create", {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          "authorization": "Bearer " + localStorage.getItem("accessToken")
-        },
-        body: JSON.stringify({
-          html: html,
-          css: css
-        })
-      })
-
-      if (!response.ok) {
-        if (response.status === 401) {
-          // Tokens are invalid. Try getting new tokens
-          if (await refreshTokens()) {
-            // Got a new token pair. Try again
-            Api.saveBlog(html, css)
-          } else {
-            // Failed to get new tokens
-            return
-          }
-        } else {
-          // Some other problem
-          console.error("Error: Unable to save blog", response.status, response.statusText)
-        }
-      }
+      await fetchWithAuth("http://localhost:8000/blog/create", "POST", { html: html, css: css }, 5)
     } catch (err) {
-      // TODO
-      console.error("Error: Could not send saveBlog request", err)
+      // Could not save blog
+      console.error(err)
     }
   }
 }
