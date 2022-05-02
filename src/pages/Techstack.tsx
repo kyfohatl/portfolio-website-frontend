9c1aa8821474727e80bf01dc82a0a08819f1e4d1
Ehsan <ehsan.soltani71@yahoo.com>
2022-05-03 02:07:41 +1000

    Added saved button svg animation as a possible button state

--- src/pages/Techstack.tsx
+++ src/pages/Techstack.tsx
@@ -1,14 +1,12 @@
+import LoadingButton from "../components/LoadingButton"
 import SavingButton from "../components/SavingButton"
-
-function onClick() {
-  console.log("You pressed the button")
-}
 
 export default function Techstack() {
   return (
     <>
       <p>This is the techstack page</p>
-      <SavingButton />
+      <SavingButton width="100px" height="40px" onAnimationEnd={() => console.log("Anim Done")} />
+      <LoadingButton width="100px" height="40px" />
     </>
   )
 }
