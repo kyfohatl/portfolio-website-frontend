58b67ebf99a48eb6f7bbd789a596d24e902fe998
Ehsan <ehsan.soltani71@yahoo.com>
2022-10-05 15:16:24 +1100

    Added missing description for Home page test

--- cypress/e2e/Home.cy.ts
+++ cypress/e2e/Home.cy.ts
@@ -1,9 +1,10 @@
 describe("The \"Explore\" button", () => {
   describe("When clicked", () => {
-    it("Blah", () => {
+    it("Scrolls the user down past the hero to the content of the page", () => {
       cy.visit("/")
       cy.get('[data-testid="exploreBtn"]').click()
       cy.window().then(function (win) {
+        // Ensure that the hero is no longer visible
         cy.get('[data-testid="heroOuterContainer"]').should("not.be.inViewport", win)
       })
     })
