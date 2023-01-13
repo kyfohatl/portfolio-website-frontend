144c96b4ad3a653791b0381899a175bc5c4c3e8e
Ehsan <ehsan.soltani71@yahoo.com>
2023-01-13 15:16:45 +1100

    Working on mobile help display carousal

--- src/components/help/mobile/HelpDisplayMobile.tsx
+++ src/components/help/mobile/HelpDisplayMobile.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react"
+import { CSSProperties, useCallback, useMemo, useState } from "react"
 import FeatureDisplayCard, { FeatureDisplayCardProps } from "../../FeatureDisplayCard"
 import HelpDisplayBackground from "../HelpDisplayBackground"
 import HelpDisplayCloseButton from "../HelpDisplayCloseButton"
@@ -10,23 +10,46 @@ interface HelpDisplayMobileProps {
 }
 
 export default function HelpDisplayMobile({ cardProps, onClose }: HelpDisplayMobileProps) {
-  const cards = useMemo(() => {
-    const cardsList = []
+  const [lefts, setLefts] = useState<number[]>(cardProps.map(() => 0))
 
-    for (const prop of cardProps) {
-      cardsList.push(<FeatureDisplayCard {...prop} />)
-    }
+  const cards = useMemo(() => {
+    const cardsList = cardProps.map((prop, i) => {
+      return (
+        <div className={styles.cardWrapper} style={{ "--distToCenterX": lefts[i] } as CSSProperties}>
+          <FeatureDisplayCard {...prop} boxShadow="0px 0px 6px 6px #C1C1C1" />
+        </div>
+      )
+    })
 
     return cardsList
-  }, [cardProps])
+  }, [cardProps, lefts])
+
+  const onScroll = useCallback((container: HTMLDivElement) => {
+    const screenCenterX = window.innerWidth / 2
+    const children = container.children
+    const newLefts: number[] = []
+
+    for (const child of children) {
+      const rect = child.getBoundingClientRect()
+      const elemCenterX = rect.left + rect.width / 2
+      newLefts.push(elemCenterX - screenCenterX)
+    }
+
+    setLefts(newLefts)
+  }, [])
 
   return (
     <div className={styles.outerContainer}>
       <HelpDisplayBackground onClose={onClose} />
 
       <div className={styles.displayContainer}>
-        <HelpDisplayCloseButton onClose={onClose} />
-        <div className={styles.cardContainer}>
+        <div className={styles.buttonContainer}>
+          <HelpDisplayCloseButton onClose={onClose} />
+        </div>
+        <div
+          className={styles.cardContainer}
+          onScroll={(e) => onScroll(e.currentTarget)}
+        >
           {cards}
         </div>
       </div>
