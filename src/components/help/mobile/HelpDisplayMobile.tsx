27d9df8cabf3df9e9d063a05acde813056c53222
Ehsan <ehsan.soltani71@yahoo.com>
2023-01-15 02:43:31 +1100

    Fixed an issue with the help display carousel where initially the cards were not set to the correct scale

--- src/components/help/mobile/HelpDisplayMobile.tsx
+++ src/components/help/mobile/HelpDisplayMobile.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, useCallback, useMemo, useState } from "react"
+import { CSSProperties, useEffect, useMemo, useRef, useState } from "react"
 import { WithRequiredType } from "../../../lib/typeHelpers/withRequiredType"
 import FeatureDisplayCard, { FeatureDisplayCardProps } from "../../FeatureDisplayCard"
 import HelpDisplayBackground from "../HelpDisplayBackground"
@@ -15,8 +15,31 @@ interface HelpDisplayMobileProps {
   onClose: () => void
 }
 
+// Calculates and sets all the X distances from the center of the screen of each child element of the given parent
+// using the given setter 
+function calculateDistances(parent: HTMLDivElement, setter: (newDistances: number[]) => void) {
+  const screenCenterX = window.innerWidth / 2
+  const children = parent.children
+  const newDists: number[] = []
+
+  for (const child of children) {
+    const rect = child.getBoundingClientRect()
+    const elemCenterX = rect.left + rect.width / 2
+    newDists.push(elemCenterX - screenCenterX)
+  }
+
+  setter(newDists)
+}
+
 export default function HelpDisplayMobile({ cardProps, onClose }: HelpDisplayMobileProps) {
-  const [lefts, setLefts] = useState<number[]>(cardProps.map(() => 0))
+  const [distToCenterXs, setDistToCenterXs] = useState<number[]>(cardProps.map(() => 0))
+
+  // Make sure that the first time the dialogue is opened, it renders the cards at the correct scale
+  const cardContainerRef = useRef(null)
+  useEffect(() => {
+    if (!cardContainerRef.current) return
+    calculateDistances(cardContainerRef.current, setDistToCenterXs)
+  }, [])
 
   const cards = useMemo(() => {
     // The distance required for a card to go from the center of the screen to completely off the screen
@@ -25,7 +48,7 @@ export default function HelpDisplayMobile({ cardProps, onClose }: HelpDisplayMob
     const cardsList = cardProps.map((prop, i) => {
       // Calculate the current scale of the card, based on how far away it is from the center of the screen
       // Cap the value to a minimum scale value
-      const curScale = Math.max((-SCALING_GRADIENT * Math.abs(lefts[i]) / offScrnDist) + 1, MIN_CARD_SCALE)
+      const curScale = Math.max((-SCALING_GRADIENT * Math.abs(distToCenterXs[i]) / offScrnDist) + 1, MIN_CARD_SCALE)
 
       return (
         <div className={styles.cardWrapper} style={{ "--curScale": curScale } as CSSProperties}>
@@ -35,21 +58,7 @@ export default function HelpDisplayMobile({ cardProps, onClose }: HelpDisplayMob
     })
 
     return cardsList
-  }, [cardProps, lefts])
-
-  const onScroll = useCallback((container: HTMLDivElement) => {
-    const screenCenterX = window.innerWidth / 2
-    const children = container.children
-    const newLefts: number[] = []
-
-    for (const child of children) {
-      const rect = child.getBoundingClientRect()
-      const elemCenterX = rect.left + rect.width / 2
-      newLefts.push(elemCenterX - screenCenterX)
-    }
-
-    setLefts(newLefts)
-  }, [])
+  }, [cardProps, distToCenterXs])
 
   return (
     <div className={styles.outerContainer}>
@@ -61,7 +70,8 @@ export default function HelpDisplayMobile({ cardProps, onClose }: HelpDisplayMob
         </div>
         <div
           className={styles.cardContainer}
-          onScroll={(e) => onScroll(e.currentTarget)}
+          onScroll={(e) => calculateDistances(e.currentTarget, setDistToCenterXs)}
+          ref={cardContainerRef}
         >
           {cards}
         </div>
