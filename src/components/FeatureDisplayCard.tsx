fb39a03d8ba27f688ac48891e3e6f83f750e0711
Ehsan <ehsan.soltani71@yahoo.com>
2023-02-09 18:39:25 +1100

    Started work on the mobile home page

--- src/components/FeatureDisplayCard.tsx
+++ src/components/FeatureDisplayCard.tsx
@@ -7,10 +7,12 @@ interface imgProps {
   width: string
 }
 
+type CardVisuals = { images: imgProps[] } | { custom: JSX.Element }
+
 export interface FeatureDisplayCardProps {
   title: string,
   notes: (string | JSX.Element)[],
-  visuals: { images: imgProps[] } | { custom: JSX.Element },
+  visuals: { desktop: CardVisuals, mobile: CardVisuals },
   theme?: "dark" | "light",
   dimensions?: { desktop: { w: string, h: string }, mobile: { w: string, h: string } }
   borderRadius?: string,
@@ -18,6 +20,17 @@ export interface FeatureDisplayCardProps {
   middleGap?: string,
   textLineSize?: string,
   noteListStyle?: "default" | "borders"
+}
+
+function createImgList(visuals: CardVisuals) {
+  let imgList: JSX.Element[] = []
+  if ("images" in visuals) {
+    imgList = visuals.images.map(
+      (image, index) => <img key={index} alt="None" src={image.imgLink} width={image.width} height={image.height} />
+    )
+  }
+
+  return imgList
 }
 
 // Uses Ref Forwarding to provide a ref access to the outer container div of this component to the parent
@@ -100,12 +113,8 @@ const FeatureDisplayCard = React.forwardRef<HTMLDivElement, FeatureDisplayCardPr
   // Create list of notes
   const notesList = notes.map((note, index) => <li key={index} style={noteStyles}>{note}</li>)
   // Create a list of images if images are given
-  let imgList: JSX.Element[] = []
-  if ("images" in visuals) {
-    imgList = visuals.images.map(
-      (image, index) => <img key={index} alt="None" src={image.imgLink} width={image.width} height={image.height} />
-    )
-  }
+  const imgListDesktop = createImgList(visuals.desktop)
+  const imgListMobile = createImgList(visuals.mobile)
 
   return (
     <div
@@ -120,10 +129,16 @@ const FeatureDisplayCard = React.forwardRef<HTMLDivElement, FeatureDisplayCardPr
           {notesList}
         </ul>
       </article>
-      <div className={styles.imageContainer}>
-        {"images" in visuals
-          ? imgList
-          : visuals.custom
+      <div className={styles.imageContainerBase + " " + styles.imgContainerDesktop}>
+        {"images" in visuals.desktop
+          ? imgListDesktop
+          : visuals.desktop.custom
+        }
+      </div>
+      <div className={styles.imageContainerBase + " " + styles.imgContainerMobile}>
+        {"images" in visuals.mobile
+          ? imgListMobile
+          : visuals.mobile.custom
         }
       </div>
     </div>
