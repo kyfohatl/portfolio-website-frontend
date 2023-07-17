73450af3682150746e8781a57ecead958e4579d0
Ehsan <ehsan.soltani71@yahoo.com>
2023-07-17 18:26:47 +1000

    Fixed incorrect logic in the isInViewPort assertion

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -51,9 +51,27 @@ function isInViewPort(this: Chai.AssertionStatic, win: Cypress.AUTWindow) {
   const element: JQuery = this._obj
   const rect = element[0].getBoundingClientRect()
 
-  const partialHeightInView = rect.bottom > 0 && rect.top < win.innerHeight
-  const partialWidthInView = rect.right > 0 && rect.left < win.innerWidth
+  // If the top of the element is somewhere on the screen, then the element must be on the screen height-wise
+  // This happens either if the element is fully in the screen, of if the top is on screen, and the bottom is below
+  // ehe screen
+  const topVisible = rect.top >= 0 && rect.top < win.innerHeight
+  // If the bottom of an element is somewhere on the screen, then the element must be on the screen height-wise
+  // This happens either if the element is fully in the screen, of if the bottom in on screen, and the top is above
+  // the screen
+  const bottomVisible = rect.bottom > 0 && rect.bottom <= win.innerHeight
+  // Finally, an element could be larger than the screen (height-wise), and thus it's top is above the screen, whilst
+  // it's bottom is below the screen, thus fully covering the height of the screen
+  const coversScreenH = rect.top <= 0 && rect.bottom >= win.innerHeight
 
+  // The same logic as above goes for the width of the screen
+  const leftVisible = rect.left >= 0 && rect.left < win.innerWidth
+  const rightVisible = rect.right > 0 && rect.right <= win.innerWidth
+  const coversScreenW = rect.left <= 0 && rect.right >= win.innerWidth
+
+  const partialHeightInView = topVisible || bottomVisible || coversScreenH
+  const partialWidthInView = leftVisible || rightVisible || coversScreenW
+
+  // For an element to be partially on screen, both it's width and height must be partially present on the screen
   this.assert(
     partialHeightInView && partialWidthInView,
     'expected #{this} to be at least partially inside the viewport, but it was not',
